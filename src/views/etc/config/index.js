4b92747a94b74d246c3a3381b7ea8e6818d3da45
wangxuancheng <1768177868@qq.com>
2026-09-20 00:16:16 +0800

    rm

--- src/views/etc/config/index.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-  apiBaseUrl: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8088',
-  port: import.meta.env.VITE_PORT || '5173',
-  // 可以添加更多配置项
-}
-
-export default config 
